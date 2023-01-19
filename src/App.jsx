33edaf5df34ce9127e3e5d19f8d5a17d7e013fee
Jesse Akorah <jesseakorah@gmail.com>
2023-01-19 14:37:12 +0100

    Create home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="main">
+        <Routes>
+          <Route path="/*" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
