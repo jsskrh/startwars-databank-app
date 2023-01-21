4dabdf9acf4f3b4e32a709710800c24bc8654146
Jesse Akorah <jesseakorah@gmail.com>
2023-01-21 14:14:05 +0100

    Setup footer template

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App h-screen">
       <Header />
-      <div className="main">
+      <div className="main flex h-full">
         <Routes>
           <Route path="/*" element={<Home />} />
         </Routes>
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+const style = {
+  footer: `px-10 py-5 flex justify-center bg-black`,
+  footerText: `text-white text-sm`,
+  boldText: `font-bold`,
+};
+
+const Footer = () => {
+  return (
+    <div className={style.footer}>
+      <p className={style.footerText}>
+        Made by <span className={style.boldText}>Jesse Akorah</span>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
