c408e65793b30c6d605c94a464230f4fb3df45e2
Jesse Akorah <jesseakorah@gmail.com>
2023-01-22 19:42:34 +0100

    Add 'No result' component to search page

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,8 @@
 import swLogo from "./star-wars-logo.webp";
 import swLogoStacked from "./star-wars-logo-stacked.webp";
 import starfield from "./starfield.webp";
+import deathStar from "./death-star.png";
 
-const assets = { swLogo, swLogoStacked, starfield };
+const assets = { swLogo, swLogoStacked, starfield, deathStar };
 
 export default assets;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,15 @@ import { useEffect, useState } from "react";
 import Pagination from "../components/Pagination";
 import PageTitle from "../components/PageTitle";
 import CharacterListItem from "../components/CharacterListItem";
+import assets from "../assets";
 
 const style = {
   container: `container text-white w-full mx-auto my-5`,
   containerInner: `mx-[4%] md:mx-[3%] pt-4 md:pt-10`,
+  emptyContainer: `mt-10 relative flex justify-center align-center`,
+  emptyImageContainer: `h-[60vh]`,
+  emptyImage: `max-h-full max-w-full opacity-20`,
+  emptyText: `absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-3xl text-[#5a5c5d]`,
 };
 
 const Search = ({ query }) => {
@@ -41,11 +46,24 @@ const Search = ({ query }) => {
       <div className={style.containerInner}>
         <div className={style.content}>
           <PageTitle title="Search Results" />
-          <ul>
-            {characters.map((character) => (
-              <CharacterListItem character={character} search />
-            ))}
-          </ul>
+          {characters.length === 0 ? (
+            <div className={style.emptyContainer}>
+              <div className={style.emptyImageContainer}>
+                <img
+                  src={assets.deathStar}
+                  alt="death star"
+                  className={style.emptyImage}
+                />
+              </div>
+              <h3 className={style.emptyText}>No result</h3>
+            </div>
+          ) : (
+            <ul>
+              {characters.map((character) => (
+                <CharacterListItem character={character} search />
+              ))}
+            </ul>
+          )}
         </div>
         <Pagination
           currentPage={currentPage}
